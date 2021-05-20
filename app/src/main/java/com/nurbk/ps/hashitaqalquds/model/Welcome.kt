ce7b05b22ec27bcc263ea5f9c7f26ad6fcc33857
OMEN <mrh61037@gmail.com>
2021-05-21 00:07:20 +0300

    update model

--- app/src/main/java/com/nurbk/ps/hashitaqalquds/model/Welcome.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/model/Welcome.kt
@@ -1,19 +1,26 @@
 package com.nurbk.ps.hashitaqalquds.model
 
 import android.content.Context
+import com.nurbk.ps.hashitaqalquds.R
 
 
-data class Welcome(var image: Int = 0, var title: String = "", var desc: String = "")
+data class Welcome(var image: Int, var title: String = "", var desc: String = "")
 
 
 fun getData(context: Context) =
     arrayListOf(
         Welcome(
-
+            R.drawable.w1,
+            context.getString(R.string.w1)
         ),
         Welcome(
-
+            R.drawable.w2,
+            context.getString(R.string.w2)
         ),
-        Welcome()
+        Welcome(
+            R.drawable.w3,
+            context.getString(R.string.w3)
+        )
+
 
     )
