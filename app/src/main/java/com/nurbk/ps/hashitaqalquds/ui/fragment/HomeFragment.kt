f9a9e6c354b23df0c563bf9a93f25de89452a45f
OMEN <mrh61037@gmail.com>
2021-05-21 00:09:25 +0300

    update home map and profile fragments

--- app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/HomeFragment.kt
@@ -1,8 +1,39 @@
 package com.nurbk.ps.hashitaqalquds.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.nurbk.ps.hashitaqalquds.R
+import com.nurbk.ps.hashitaqalquds.databinding.FragmentHomeBinding
+import com.nurbk.ps.hashitaqalquds.other.setToolbarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+    private val mBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = mBinding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().setToolbarView(
+            mBinding.toolbarView,
+            title = getString(R.string.home),
+            isMane = true,
+            idMenu = R.drawable.ic_add
+        ) {
+
+        }
+    }
+
 }
--- app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/MapFragment.kt
@@ -1,8 +1,39 @@
 package com.nurbk.ps.hashitaqalquds.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nurbk.ps.hashitaqalquds.R
+import com.nurbk.ps.hashitaqalquds.databinding.FragmentHomeBinding
+import com.nurbk.ps.hashitaqalquds.databinding.FragmentMapBinding
+import com.nurbk.ps.hashitaqalquds.other.setToolbarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MapFragment : Fragment(){
+
+    private val mBinding by lazy {
+        FragmentMapBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = mBinding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().setToolbarView(
+            mBinding.toolbarView,
+            title = getString(R.string.map),
+            isMane = true,
+            idMenu = R.drawable.ic_replay
+        ) {
+
+        }
+    }
 }
