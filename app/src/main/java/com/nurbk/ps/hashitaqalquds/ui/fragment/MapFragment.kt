660af44e6c7fc34a9b0b016238c47f221ac0aa49
OMEN <mrh61037@gmail.com>
2021-05-20 15:10:39 +0300

    add ui frament home map profile

---
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/MapFragment.kt
@@ -0,0 +1,8 @@
+package com.nurbk.ps.hashitaqalquds.ui.fragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MapFragment : Fragment(){
+}
