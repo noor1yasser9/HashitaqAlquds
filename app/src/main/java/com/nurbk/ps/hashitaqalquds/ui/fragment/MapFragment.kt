036233e0891f91cfed945d13d94dce2c355da90c
OMEN <mrh61037@gmail.com>
2021-05-24 12:27:18 +0300

    update map

--- app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/MapFragment.kt
@@ -1,10 +1,21 @@
 package com.nurbk.ps.hashitaqalquds.ui.fragment
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.nurbk.ps.hashitaqalquds.R
 import com.nurbk.ps.hashitaqalquds.databinding.FragmentHomeBinding
 import com.nurbk.ps.hashitaqalquds.databinding.FragmentMapBinding
@@ -12,11 +23,13 @@ import com.nurbk.ps.hashitaqalquds.other.setToolbarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MapFragment : Fragment(){
+class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private val mBinding by lazy {
         FragmentMapBinding.inflate(layoutInflater)
     }
+    private lateinit var mCustomMarkerView: View
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +40,13 @@ class MapFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        mCustomMarkerView = (requireActivity().getSystemService(
+            Context.LAYOUT_INFLATER_SERVICE
+        ) as LayoutInflater).inflate(R.layout.view_custom_marker, null)
+
+
         requireActivity().setToolbarView(
             mBinding.toolbarView,
             title = getString(R.string.map),
@@ -35,5 +55,48 @@ class MapFragment : Fragment(){
         ) {
 
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.setPadding(20, 30, 30, 340)
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMarkerClickListener(this)
+        mMap.setOnMapClickListener {
+            val userMarker = mMap.addMarker(
+                MarkerOptions()
+                    .position(it)
+                    .title("Your Here")
+            )
+
+            userMarker!!.setIcon(
+                BitmapDescriptorFactory.fromBitmap(
+                    getMarkerBitmapFromView(
+                        mCustomMarkerView
+                    )!!
+                )
+            )
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        return false
+    }
+
+    private fun getMarkerBitmapFromView(view: View): Bitmap? {
+//        mMarkerImageView.setImageBitmap(bitmap)
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+        view.buildDrawingCache()
+        val returnedBitmap = Bitmap.createBitmap(
+            view.measuredWidth, view.measuredHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(returnedBitmap)
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN)
+        val drawable = view.background
+        drawable?.draw(canvas)
+        view.draw(canvas)
+        return returnedBitmap
     }
 }
