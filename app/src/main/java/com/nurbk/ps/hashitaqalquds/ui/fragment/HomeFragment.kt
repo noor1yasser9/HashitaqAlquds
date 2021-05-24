32724e306bde8c209a7022c589d97b3c1577bc43
OMEN <mrh61037@gmail.com>
2021-05-24 13:39:05 +0300

    add item image home post

--- app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/ui/fragment/HomeFragment.kt
@@ -6,15 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.nurbk.ps.hashitaqalquds.BR
 import com.nurbk.ps.hashitaqalquds.R
+import com.nurbk.ps.hashitaqalquds.adapter.GenericAdapter
 import com.nurbk.ps.hashitaqalquds.databinding.FragmentHomeBinding
+import com.nurbk.ps.hashitaqalquds.model.Welcome
+import com.nurbk.ps.hashitaqalquds.model.getData
 import com.nurbk.ps.hashitaqalquds.other.setToolbarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() ,GenericAdapter.OnListItemViewClickListener<Welcome> {
     private val mBinding by lazy {
         FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    private val mAdapter by lazy {
+        GenericAdapter(R.layout.item_post_image, BR.welcome, this)
     }
 
     override fun onCreateView(
@@ -34,6 +42,16 @@ class HomeFragment : Fragment() {
         ) {
             findNavController().navigate(R.id.action_homeFragment_to_dialogAddPost)
         }
+
+        mAdapter.submitList(getData(requireContext()))
+
+        mBinding.rcData.apply {
+            adapter = mAdapter
+        }
+
     }
 
+    override fun onClickItem(itemViewModel: Welcome, type: Int) {
+
+    }
 }
