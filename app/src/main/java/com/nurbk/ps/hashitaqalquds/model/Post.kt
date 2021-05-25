0002932a8e4588b7e8a140258111d1526d98de0d
omarmattr <omarabomattr@gmail.com>
2021-05-25 16:49:47 +0300

    add Result.kt class and add functions on MainViewModel.kt

--- app/src/main/java/com/nurbk/ps/hashitaqalquds/model/Post.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/model/Post.kt
@@ -2,11 +2,18 @@ package com.nurbk.ps.hashitaqalquds.model
 
 data class Post(
     val id: String = "",
-    val userId:String="",
-    val userName:String="",
-    val userImage:String="",
+    val userId: String = "",
+    val userName: String = "",
+    val userImage: String = "",
     val date: String = "",
     val tag: String = "",
     val content: String = "",
-    val media: String = ""
-)
+    val media: String = "",
+    val mediaType: Int
+){
+    companion object{
+        const val PHOTO_TYPE = 0
+        const val VIDEO_TYPE = 1
+        const val PDF_TYPE = 2
+    }
+}
