0b7c481edfb27903828966a20955d28b467ebddb
OMEN <mrh61037@gmail.com>
2021-05-26 02:35:16 +0300

    update model post

--- app/src/main/java/com/nurbk/ps/hashitaqalquds/model/Post.kt
+++ app/src/main/java/com/nurbk/ps/hashitaqalquds/model/Post.kt
@@ -3,17 +3,16 @@ package com.nurbk.ps.hashitaqalquds.model
 data class Post(
     val id: String = "",
     val userId: String = "",
-    val userName: String = "",
-    val userImage: String = "",
-    val date: String = "",
+    val date: Long = 0,
     val tag: String = "",
     val content: String = "",
-    val media: String = "",
-    val mediaType: Int
-){
-    companion object{
+    var media: String = "",
+    val type: Int
+) {
+    companion object {
         const val PHOTO_TYPE = 0
         const val VIDEO_TYPE = 1
         const val PDF_TYPE = 2
+        const val CONTENT_TYPE = 4
     }
 }
